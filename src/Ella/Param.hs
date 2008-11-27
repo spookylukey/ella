f80fa488b01bea18ef66fbb62375c6633799f056
Luke Plant <L.Plant.98@cantab.net>
2008-11-27 00:20:57 +0000

    Added utlity parsing functions

--- src/Ella/Param.hs
+++ src/Ella/Param.hs
@@ -1,9 +1,12 @@
 {-# OPTIONS_GHC -XTypeSynonymInstances #-}
 module Ella.Param ( Param(..)
+                  , parseOrDefault
+                  , captureOrDefault
                   )
 
 where
 
+import Maybe (fromMaybe)
 import Ella.GenUtils (exactParse)
 
 -- | Type class representing parameters that can be captured (usually
@@ -20,3 +23,19 @@ instance Param Integer where
 
 instance Param String where
     capture = Just
+
+-- | Parse a value (packed in a Just) or return a default
+--
+-- This is useful in dealing with 'Maybe' vals returned from getGET,
+-- getPOST etc.  It can be used with any types that are instances of Read.
+-- Note that 'Read' for Strings expects quotes and escaping, so this
+-- in general is not useful for String values.
+parseOrDefault :: (Read a) => Maybe String -> a -> a
+parseOrDefault v d = fromMaybe d (v >>= exactParse)
+
+-- | Parse a value (packed in a Just) or return a default
+--
+-- This is useful in dealing with 'Maybe' vals returned from getGET,
+-- getPOST etc.  It can be used with any types that are instances of Param.
+captureOrDefault :: (Param a) => Maybe String -> a -> a
+captureOrDefault v d = fromMaybe d (v >>= capture)
