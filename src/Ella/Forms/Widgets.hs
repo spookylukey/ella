ce2122026adea4212a21750c215690e6c94bd255
Luke Plant <L.Plant.98@cantab.net>
2008-12-03 23:57:05 +0000

    Support for control ids and names

--- src/Ella/Forms/Widgets.hs
+++ src/Ella/Forms/Widgets.hs
@@ -1,31 +1,72 @@
 module Ella.Forms.Widgets where
 
+import Control.Monad (liftM)
+import Data.Maybe (catMaybes)
 import qualified Text.XHtml as X
 import Text.XHtml ( (<<)
                   , (+++)
                   , (!)
                   )
 
+-- Classes --
+class HasId a where
+    setId :: String -> a -> a
+    getId :: a -> Maybe String
+    removeId :: a -> a
+
+
+-- Widgets --
 data TextInput = TextInput {
       defaultVal :: String
     , maxlength :: Maybe Int
     , size :: Maybe Int
+    , name :: String
+    , identifier :: Maybe String
     }
+
 
 emptyTextInput = TextInput { defaultVal = ""
                            , maxlength = Nothing
                            , size = Nothing
+                           , name = ""
+                           , identifier = Nothing
                            }
+
+data Label a = Label {
+      text :: X.Html
+    , control :: Maybe a
+    }
+
+-- HTML instances --
 
 instance X.HTML TextInput where
     toHtml t = let attrs =  [ X.thetype "text"
+                            , X.name $ name t
                             , X.value $ defaultVal t ] ++
-                          case maxlength t of
-                            Nothing -> []
-                            Just ml -> [X.maxlength ml]
-                           ++
-                          case size t of
-                            Nothing -> []
-                            Just s -> [X.size $ show s]
+                          catMaybes [ liftM X.identifier $ identifier t
+                                    , liftM X.maxlength $ maxlength t
+                                    , liftM (X.size . show) $ size t
+                                    ]
                in X.input ! attrs
 
+instance (HasId a) => X.HTML (Label a) where
+    toHtml l = let attrs = case control l of
+                             Nothing -> []
+                             Just c -> case getId c of
+                                         Nothing -> []
+                                         Just theid -> [ X.thefor theid ]
+               in X.label ! attrs << text l
+
+-- HasId instances
+
+instance HasId TextInput where
+    setId theid t = t { identifier = Just theid }
+    getId t = identifier t
+    removeId t = t { identifier = Nothing }
+
+
+-- Utilities
+makeLabel :: (HasId a) => String -> a -> Label a
+makeLabel t c = Label { text = X.toHtml t
+                      , control = Just c
+                      }
--- testsuite/Tests/Ella/Forms/Widgets.hs
+++ testsuite/Tests/Ella/Forms/Widgets.hs
@@ -9,19 +9,23 @@ import Ella.Forms.Widgets
 
 render = X.showHtmlFragment . X.toHtml
 
-testTextInputRender_1 = "<input type=\"text\" value=\"\" />" ~=? render emptyTextInput
-testTextInputRender_2 = "<input type=\"text\" value=\"foo\" />" ~=? (render $ emptyTextInput { defaultVal = "foo" })
-testTextInputRender_3 = "<input type=\"text\" value=\"foo\" maxlength=\"20\" size=\"10\" />" ~=?
+testTextInputRender_1 = "<input type=\"text\" name=\"\" value=\"\" />" ~=? render emptyTextInput
+testTextInputRender_2 = "<input type=\"text\" name=\"foo\" value=\"foo\" />" ~=? (render $ emptyTextInput { defaultVal = "foo", name = "foo" })
+testTextInputRender_3 = "<input type=\"text\" name=\"aname\" value=\"foo\" id=\"anid\" maxlength=\"20\" size=\"10\" />" ~=?
                         (render $ TextInput { defaultVal = "foo"
                                             , size = Just 10
                                             , maxlength = Just 20
+                                            , name = "aname"
+                                            , identifier = Just "anid"
                                             })
-testTextInputAddAttributes_1 = "<input type=\"text\" value=\"\" class=\"foo\" />" ~=?
+testTextInputAddAttributes_1 = "<input type=\"text\" name=\"\" value=\"\" class=\"foo\" />" ~=?
                                (X.showHtmlFragment $ (X.toHtml emptyTextInput) ! [X.theclass "foo"])
 
+testTextInputRender_4 = "<input type=\"text\" name=\"foo\" value=\"\" id=\"id_foo\" />" ~=? (render $ setId "id_foo" $ emptyTextInput { name = "foo" })
 
 tests = test [ testTextInputRender_1
              , testTextInputRender_2
              , testTextInputRender_3
              , testTextInputAddAttributes_1
+             , testTextInputRender_4
              ]
